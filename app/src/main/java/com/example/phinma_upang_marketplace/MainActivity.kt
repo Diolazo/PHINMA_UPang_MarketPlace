a82c824ae02e551629c1e1ec0b9d7fd0513c6070
Diolazo <joey.diolazo.up@phinmaed.com>
2024-02-01 18:04:41 +0800

    New XML Layout for Signup

--- app/src/main/java/com/example/phinma_upang_marketplace/MainActivity.kt
+++ app/src/main/java/com/example/phinma_upang_marketplace/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.phinma_upang_marketplace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var signview: TextView
+
+<<<<<<< HEAD
+class MainActivity : AppCompatActivity() {
+=======
+>>>>>>> f5eb8a2 (New XML Layout and code for going into sign up page)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        signview.setOnClickListener{
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/phinma_upang_marketplace/SignupActivity.kt
@@ -0,0 +1,7 @@
+package com.example.phinma_upang_marketplace
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SignupActivity : AppCompatActivity() {
+
+}
