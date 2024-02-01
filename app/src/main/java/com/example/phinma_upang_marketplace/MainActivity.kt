8d5326eb5c3d2ba87876e5ff8b6e6ba3dda687e6
Hyudz <fraionhyudz10@gmail.com>
2024-02-02 05:55:23 +0800

    fixed errors

--- app/src/main/java/com/example/phinma_upang_marketplace/MainActivity.kt
+++ app/src/main/java/com/example/phinma_upang_marketplace/MainActivity.kt
@@ -6,19 +6,13 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var signview: TextView
-
-<<<<<<< HEAD
-class MainActivity : AppCompatActivity() {
-=======
->>>>>>> f5eb8a2 (New XML Layout and code for going into sign up page)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        signview = findViewById<TextView>(R.id.Textview2)
 
-
-        signview.setOnClickListener{
+        signview.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
